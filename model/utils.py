89fd52d6f81d286a51ff3e984ece1538fb769cfe
VillardX <43296123+VillardX@users.noreply.github.com>
2022-08-17 19:35:26 +0800

    Update utils.py

--- model/utils.py
+++ model/utils.py
@@ -10,20 +10,6 @@ from transformers import AutoTokenizer
 TIME_STAMP = datetime.now().strftime('%Y-%m-%dT%H:%M')
 
 import re
-# import emoji
-# from soynlp.normalizer import repeat_normalize
-
-# emojis = ''.join(emoji.UNICODE_EMOJI.keys())
-# pattern = re.compile(f'[^ .,?!/@$%~％·∼()\x00-\x7Fㄱ-힣{emojis}]+')
-# url_pattern = re.compile(
-#     r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)')
-
-# def clean(x):
-#     x = pattern.sub(' ', x)
-#     x = url_pattern.sub('', x)
-#     x = x.strip()
-#     x = repeat_normalize(x, num_repeats=2)
-#     return x
 
 def load_tokenizer(args):
     return AutoTokenizer.from_pretrained(args.model_name_or_path)
